8ffc6d78f26dd18ac11d47ce5da58258f9c02da4
johnny.hyland <j.hyland.2@research.gla.ac.uk>
2022-05-24 11:25:23 +0100

    starting to move functions into .py files

--- p_astro_utils.py
+++ p_astro_utils.py
@@ -1,1 +1,14 @@
 """A place for storing the functions required for the P_astro analysis"""
+
+import numpy as np
+
+
+def cdf(prob_dist):
+    """Compute the normalised CDF of the probabilioty distribution x.
+    :param prob_dist: The probability distribution of interest.
+    :returns: The normalised cumulative distribution of the input probability distribution.
+    """
+    cdf = np.cumsum(prob_dist).to_numpy() # sum all the probabilities
+    cdf = (cdf - cdf[0]) # subtract the first value so we start from zero
+    cdf /= cdf[-1] # divide by the last value to normalise to 1
+    return cdf
